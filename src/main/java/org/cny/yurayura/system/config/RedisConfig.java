5c6617ec7b10e294d7ab2352473a46b1b39d233d
cny <elianacc@126.com>
2021-04-27 14:14:39 +0800

    fix: redis配置备注修改

--- src/main/java/org/cny/yurayura/system/config/RedisConfig.java
+++ src/main/java/org/cny/yurayura/system/config/RedisConfig.java
@@ -25,12 +25,17 @@ public class RedisConfig {
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
         RedisTemplate<String, Object> template = new RedisTemplate<String, Object>();
         template.setConnectionFactory(factory);
+
+        // Json序列化配置
         Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
         ObjectMapper om = new ObjectMapper();
         om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
         om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
         jackson2JsonRedisSerializer.setObjectMapper(om);
+
+        // String的序列化
         StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
+
         // key采用String的序列化方式
         template.setKeySerializer(stringRedisSerializer);
         // hash的key也采用String的序列化方式
